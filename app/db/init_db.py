d914d715abdf5617139c4ea338a0855dd7eb900a
bach <nxb152@gmail.com>
2025-07-21 15:02:24 +0700

    fix tiep

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,7 +1,8 @@
 # Table
 from app.models.user import User
+from app.models.base import BaseModel
 
 from app.db.database import SessionLocal, engine, Base
 
 def init_database():
-    Base.metadata.create_all(bind=engine)
+    BaseModel.metadata.create_all(bind=engine)
--- app/models/base.py
+++ app/models/base.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, func, DateTime
 from app.db.database import Base
 
 
 class BaseModel(Base):
     __abstract__ = True
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    created_at = Column(String, nullable=False)
-    updated_at = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
